b4b6a2013adacc1d9c52fa6e03a5209b6e0eeb7d
HuBoChin <51033102@gm.nfu.edu.tw>
2022-06-08 16:35:01 +0800

    token1

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n 操作練習 \n [h1] \n token產生 \n [/h1] \n [h1] \n token使用 \n [/h1] \n', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site \n \n 操作練習 \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'token產生', 'text': '\n', 'tags': '', 'url': 'token產生.html'}, {'title': 'token使用', 'text': '\n', 'tags': '', 'url': 'token使用.html'}]};
